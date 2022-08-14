7a5d6a5e58eaa6fea8764c5c5df00e5dc1b45b03
Pratik Mohan <77787014+Pratik-Mohan@users.noreply.github.com>
2022-08-14 22:51:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
+import os
 from plyer import notification
 
 if __name__ == "__main__":
     while True:
         notification.notify(
-            title="**Please Drink Water Now!",
-            message="how much fluid does the average, healthy adult living in a temperate climate need? The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
-            app_icon="./favi.ico",
+            title="Please Drink Water Now!",
+            message="how much fluid does the average, healthy adult living in a temperate climate need? The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day.",
+            app_icon=os.getcwd() + "\\favi.ico",
             timeout=12
         )
+        #Alerts after every hour
         time.sleep(60*60)
